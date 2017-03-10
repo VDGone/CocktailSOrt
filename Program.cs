4fcc60c6e9084a02a1c8d2f85f0aceb68b4d8cfb
VDGone <disasterpiece788@gmail.com>
2017-03-10 12:07:02 +0200

    practice

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace CocktailSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numsToSort = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int start = 0;
+            int end = numsToSort.Length - 1;
+            int temp;
+            bool swapped = true;
+
+            while (swapped)
+            {
+                swapped = false;
+
+                for (int i = 0; i < end; i++)
+                {
+                    if (numsToSort[i] > numsToSort[i + 1])
+                    {
+                        temp = numsToSort[i];
+                        numsToSort[i] = numsToSort[i + 1];
+                        numsToSort[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+
+                swapped = false;
+                end -= 1;
+
+                for (int i = end; i >= start; i--)
+                {
+                    if (numsToSort[i] > numsToSort[i + 1])
+                    {
+                        temp = numsToSort[i];
+                        numsToSort[i] = numsToSort[i + 1];
+                        numsToSort[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                start += 1;
+            }
+
+            Console.WriteLine(string.Join(", ", numsToSort));
+        }
+    }
+}
